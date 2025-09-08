6e45399441277bb0fe27b2f5f445dabc04302815
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-09-08 20:06:50 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+let max = 20;
+let random = Math.floor(Math.random() * 20) + 1;
+console.log(random);
+
+const num3 = 28;
+const num4 = 6;
+
+let mod = num3 % num4;
+console.log(mod);
